ed300c1069b0a0e31c7fb10cf15670e01d6181e5
hoguc <hoguchi@oguchiharukanoMacBook-Pro.local>
2019-11-03 18:12:42 +0900

    PostControllerAPIの生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 5.times do |i|
   Post.create!(
     title: "Pet#{i}",
-    image: open("#{Rails.root}/db/fixtures/dog_image1.jpg"),
+    image: open("#{Rails.root}/db/fixtures/pet_image#{i}.jpg"),
     content: "うちのPet#{i}が可愛いすぎる"
   )
 end
