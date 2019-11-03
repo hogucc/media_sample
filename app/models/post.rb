c07991ebea67be3e149f1b41cc46b8ad1e673ac7
hoguc <hoguchi@oguchiharukanoMacBook-Pro.local>
2019-11-03 15:05:36 +0900

    postモデルにサンプルデータを投入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# ペットの記事モデルクラス
 class Post < ApplicationRecord
+  mount_uploader :image, ImagesUploader
   validates :title, presence: true, length: { maximum: 20 }
   validates :image, presence: true
   validates :content, presence: true, length: { maximum: 140 }
