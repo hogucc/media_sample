19bad7927373ea889ad28459fc56c5c0803801c1
hoguc <hoguchi@oguchiharukanoMacBook-Pro.local>
2019-11-10 12:08:27 +0900

    bundle updateを実行

--- vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.5/ports/x86_64-apple-darwin18.7.0/libxml2/2.9.10/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/hoguchi/media_sample/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.5/ports/x86_64-apple-darwin18.7.0/libxml2/2.9.10/lib"
-XML2_LIBS="-lxml2 -lz -L/usr/local/Cellar/xz/5.2.4/lib -llzma -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/hoguchi/media_sample/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.5/ports/x86_64-apple-darwin18.7.0/libxml2/2.9.10/include/libxml2"
-MODULE_VERSION="xml2-2.9.10"
-
