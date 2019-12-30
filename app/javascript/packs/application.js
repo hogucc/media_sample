ad8052576a285dc9a0475d9952267da597e57dcd
hoguc <hoguchi@oguchiharukanoMacBook-Pro.local>
2019-12-30 16:39:10 +0900

    カードをクリックすると投稿内容の詳細が表示される機能を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Router from '../router/router.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 const app = new Vue({
   el: '#app',
--- app/javascript/pages/PostHome.vue
+++ app/javascript/pages/PostHome.vue
@@ -1,34 +1,31 @@
 <template>
-  <div class="container js-postsIndex">
-  <div class="row">
-    <div class="col s7 m7" v-for="post in posts" v-bind:key="post.id">
-      <div class="card">
-        <div class="card-image">
-          <img :src="post.image.url"/>
-        </div>
-        <div class="card-content">
-          <span class="card-title" v-on:click="setPostInfo(post.id)">
-            <a id="card-title-link" href="#">{{post.title}}</a>
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row" v-show="postInfoBool">
-    <div class="col s12 m12">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <div class="card-title">
-            {{ postInfo.title }}
+  <div class="container">
+    <div class="row">
+      <div class="col s7 m7" v-for="(post, index) in posts" v-bind:key="post.id">
+        <div class="card">
+          <div class="card-image">
+            <img :src="post.image.url"/>
           </div>
-          <div class="detail">
-            {{ postInfo.content }}
+          <div class="card-content" v-on:click="toggleShowPost(index)">
+            <div class="card-title">
+              {{post.title}}
+            </div>
+            <div class="card-more">
+              <div v-if="show[index]">
+                <font-awesome-icon icon="angle-up" size="lg"/>
+              </div>
+              <div v-else>
+                <font-awesome-icon icon="angle-down" size="lg"/>
+              </div>
+            </div>
+          </div>
+          <div class="card-detail" v-show="show[index]">
+            {{post.content}}
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -38,9 +35,8 @@
     name: 'PostHome',
     data: function() {
       return{
-        postInfo: {},
-        postInfoBool: false,
         posts: [],
+        show: {}
       }
     },
     mounted: function(){
@@ -56,12 +52,8 @@
           console.log(error);
         });
       },
-      setPostInfo(id){
-        axios.get(`api/v1/posts/${id}.json`).then(res => {
-          console.log(res.data)
-          this.postInfo = res.data;
-          this.postInfoBool = true;
-        });
+      toggleShowPost(key){
+        this.$set(this.show , key, !this.show[key])
       }
     }
   }
