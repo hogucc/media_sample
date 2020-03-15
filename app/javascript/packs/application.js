09ec585b37c097fed924c0c1b176b4645a59c16b
hogucc <uku.h1r8@gmail.com>
2020-03-15 20:15:12 +0900

    materializeの設定を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-//= require materialize
-
 library.add(fas)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
