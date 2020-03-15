a57aea3ea53ceebf9b950d12b570fbeeb0bc9650
hogucc <uku.h1r8@gmail.com>
2020-03-15 19:59:39 +0900

    materializeの設定を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+//= require materialize
+
 library.add(fas)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
